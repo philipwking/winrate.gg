0ac977bec8718d0d15a5b41efacade0cf71aa4ef
philipwking <42879808+philipwking@users.noreply.github.com>
2020-06-12 19:37:57 -0500

    Added graphs!

--- client/src/pages/GameDetails.js
+++ client/src/pages/GameDetails.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom"
 import { useStoreContext } from "../utils/GlobalState"
 import findChampion from "../utils/champions"
 import NavBar from "../components/NavBar"
+import Chart from "react-apexcharts"
+
+
 
 function GameDetails() {
     const [state, dispatch] = useStoreContext();
@@ -50,6 +53,96 @@ function GameDetails() {
 
         return user;
     }
+
+    const options1 = {
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded'
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: [findUsername(mainPlayer.participantId), findUsername(team1[0].participantId), findUsername(team1[1].participantId), findUsername(team1[2].participantId), findUsername(team1[3].participantId)]
+        },
+        yaxis: {
+            title: {
+                text: 'Kills/Deaths/Assists'
+            }
+        },
+        fill: {
+            opacity: 1
+        }
+    }
+    const series1 = [
+        {
+            name: 'Kills',
+            data: [mainPlayer.stats.kills, team1[0].stats.kills, team1[1].stats.kills, team1[2].stats.kills, team1[3].stats.kills]
+        }, {
+            name: 'Deaths',
+            data: [mainPlayer.stats.deaths, team1[0].stats.deaths, team1[1].stats.deaths, team1[2].stats.deaths, team1[3].stats.deaths]
+        }, {
+            name: 'Assists',
+            data: [mainPlayer.stats.assists, team1[0].stats.assists, team1[1].stats.assists, team1[2].stats.assists, team1[3].stats.assists]
+        }
+    ]
+    const options2 = {
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded'
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: [findUsername(team2[0].participantId), findUsername(team2[1].participantId), findUsername(team2[2].participantId), findUsername(team2[3].participantId), findUsername(team2[3].participantId)]
+        },
+        yaxis: {
+            title: {
+                text: 'Kills/Deaths/Assists'
+            }
+        },
+        fill: {
+            opacity: 1
+        }
+    }
+    const series2 = [
+        {
+            name: 'Kills',
+            data: [team2[0].stats.kills, team2[1].stats.kills, team2[2].stats.kills, team2[3].stats.kills, team2[4].stats.kills]
+        }, {
+            name: 'Deaths',
+            data: [team2[0].stats.deaths, team2[1].stats.deaths, team2[2].stats.deaths, team2[3].stats.deaths, team2[4].stats.deaths]
+        }, {
+            name: 'Assists',
+            data: [team2[0].stats.assists, team2[1].stats.assists, team2[2].stats.assists, team2[3].stats.assists, team2[4].stats.assists]
+        }
+    ]
+
 
 
     useEffect(() => {
@@ -108,6 +201,9 @@ function GameDetails() {
                                     ))
                                 }
                             </ul>
+                            <div id="chart">
+                                <Chart options={options1} series={series1} type="bar" height={350} />
+                            </div>
                         </div>
                         <div className={!mainPlayer.stats.win ? "win" : "loss"}>
                             Enemy Team
@@ -134,6 +230,9 @@ function GameDetails() {
                                     ))
                                 }
                             </ul>
+                            <div id="chart">
+                                <Chart options={options2} series={series2} type="bar" height={350} />
+                            </div>
                         </div>
                     </div>
                 </div>
