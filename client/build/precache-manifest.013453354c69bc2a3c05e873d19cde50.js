a807b005dd876c923c871c2b4c0f5f58e056b4fd
philipwking <42879808+philipwking@users.noreply.github.com>
2020-06-23 15:01:53 -0500

    publishing build for vercel deployment

---
+++ client/build/precache-manifest.013453354c69bc2a3c05e873d19cde50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857fd43f4720d69107c9cc93c0c2b202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abf9c349cc248fb63c64",
+    "url": "/static/css/main.c7bf2990.chunk.css"
+  },
+  {
+    "revision": "7da63b3cedc6a3daa57e",
+    "url": "/static/js/2.06a899e0.chunk.js"
+  },
+  {
+    "revision": "953ddbfd33e29cfe417b4c1451a30c50",
+    "url": "/static/js/2.06a899e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf9c349cc248fb63c64",
+    "url": "/static/js/main.cb92de94.chunk.js"
+  },
+  {
+    "revision": "20c573aaaa2d44c1c4a3",
+    "url": "/static/js/runtime-main.5908aa69.js"
+  }
+]);
